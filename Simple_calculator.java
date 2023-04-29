899d331b24df501083e200d122a58b3b0824cec5
divyeshpanchasara <114856928+divyeshpanchasara@users.noreply.github.com>
2023-04-29 11:52:37 +0530

    Add files via upload

---
+++ Simple_calculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Simple_calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Number:");
+        Double a = sc.nextDouble();
+        System.out.print("Enter Other Number:");
+        Double b = sc.nextDouble();
+        System.out.print("Enter The Operation + - * /:");
+        char c = sc.next().charAt(0);
+        sc.close();
+        if(c=='+'){
+            System.out.printf("The sum is %f",a+b);
+        }
+        else if(c=='-'){
+            System.out.printf("The difference is %f",a-b);
+        }
+        else if(c=='*'){
+            System.out.printf("The difference is %f",a*b);
+        }
+        else if(c=='/'){
+            System.out.printf("The difference is %f",(Double)a/(Double)b);
+        }
+
+    }
+}
